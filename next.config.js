99e1805dadd5c6a0303228f2af3a6a00a7b182d6
Thomas Hoang <megatunger@gmail.com>
2023-06-12 04:23:15 +0700

    add api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          ...config.resolve.fallback,
+          fs: false,
+        },
+      };
+    }
+    config.module = {
+      ...config.module,
+      exprContextCritical: false,
+    };
+    return config;
+  },
+};
+module.exports = nextConfig;
---
+++ src/app/api/instructions/route.ts
@@ -0,0 +1,86 @@
+import { Elusiv, TokenType, TopupTxData } from "@elusiv/sdk";
+import {
+  Cluster,
+  clusterApiUrl,
+  Connection,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction,
+} from "@solana/web3.js";
+import { NextResponse } from "next/server";
+import bs58 from "bs58";
+import base58 from "bs58";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const cluster = searchParams.get("cluster") || "devnet";
+  const seed = searchParams.get("seed");
+  const token = searchParams.get("token");
+  const amount = parseFloat(searchParams.get("amount") || "0.0");
+
+  const connection = new Connection(clusterApiUrl(cluster));
+  const userPublicKey = searchParams.get("userPublicKey");
+  const elusiv = await Elusiv.getElusivInstance(
+    bs58.decode(seed!),
+    new PublicKey(userPublicKey!),
+    connection,
+    cluster as Cluster
+  );
+
+  const topupTxData = await elusiv.buildTopUpTx(
+    LAMPORTS_PER_SOL * amount,
+    token as TokenType
+  );
+  console.log(
+    "typeof",
+    topupTxData.commitmentHash,
+    typeof topupTxData.commitmentHash
+  );
+
+  const transaction = topupTxData.tx;
+  const serializedTransaction = transaction.serialize({
+    verifySignatures: false,
+    requireAllSignatures: false,
+  });
+  const base64Transaction = serializedTransaction.toString("base64");
+
+  return NextResponse.json({
+    transaction: base64Transaction,
+    topupTxData: {
+      ...topupTxData,
+      commitmentHash: base58.encode(topupTxData.commitmentHash),
+    },
+  });
+}
+
+export async function POST(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const { topupTxData, signedTransaction } = await request.json();
+  const cluster = searchParams.get("cluster") || "devnet";
+  const seed = searchParams.get("seed");
+  const connection = new Connection(clusterApiUrl(cluster));
+  const userPublicKey = searchParams.get("userPublicKey");
+  const transactionEncodedStr = signedTransaction;
+  const elusiv = await Elusiv.getElusivInstance(
+    bs58.decode(seed!),
+    new PublicKey(userPublicKey!),
+    connection,
+    cluster as Cluster
+  );
+  console.log(topupTxData);
+  const transactionDecoded = new Buffer(transactionEncodedStr!, "base64");
+  const transaction = Transaction.from(transactionDecoded);
+  const topup = new TopupTxData(
+    topupTxData.fee,
+    topupTxData.tokenType,
+    topupTxData.lastNonce,
+    bs58.decode(topupTxData.commitmentHash),
+    topupTxData.merkleStartIndex,
+    topupTxData.wardenInfo,
+    transaction,
+    topupTxData.hashAccIndex,
+    topupTxData.merge
+  );
+  const signature = await elusiv.sendElusivTx(topup);
+  return NextResponse.json({ signature: signature });
+}
